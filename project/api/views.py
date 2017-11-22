10eec4af9ef400d5a1391623dec33056dfd9205e
marciomarquessouza <marciomultimedia@gmail.com>
2017-11-21 23:42:14 -0200

    tested and created a new route - add_user

--- project/api/views.py
+++ project/api/views.py
@@ -1,6 +1,8 @@
 #project/api/views.py
 
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify,request
+from project.api.models import User
+from project import db
 
 users_blueprint = Blueprint('users',__name__)
 
@@ -10,3 +12,22 @@ def ping_pong():
         'status': 'success',
         'message': 'pong!'
     })
+
+@users_blueprint.route('/users',methods=['POST'])
+def add_user():
+    #Recovering the request data
+    post_data = request.get_json()
+    username = post_data.get('username')
+    email = post_data.get('email')
+
+    #Recording the data received in the db
+    db.session.add(User(username=username,email=email))
+    db.session.commit()
+
+    #Sending a success response
+    response_object = {
+        'status': 'success',
+        'message': f'{email} was added!'
+    }
+
+    return jsonify(response_object),201
