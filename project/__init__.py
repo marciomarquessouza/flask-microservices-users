1f0a95dcb529132a1d1a827c45aca0868fcc5131
marciomarquessouza <marciomultimedia@gmail.com>
2017-11-21 22:48:50 -0200

    blueprint and model

--- project/__init__.py
+++ project/__init__.py
@@ -1,48 +1,27 @@
+
 #project/__init__.py
 
 import os
-import datetime
-from flask import Flask, jsonify
+from flask import Flask,jsonify
 from flask_sqlalchemy import SQLAlchemy
 
+#instance the db
+db = SQLAlchemy()
 
-# instantiate the app
-app = Flask(__name__)
+def create_app():
 
-# set config
-app_settings = os.getenv('APP_SETTINGS')
-db_settings  = os.getenv('POSTGRES_USER')
-app.config.from_object(app_settings)
+    #instantiate the app
+    app = Flask(__name__)
 
-# instantiate the db
-db = SQLAlchemy(app)
+    #set config
+    app_settings = os.getenv('APP_SETTINGS')
+    app.config.from_object(app_settings)
 
-# model
-class User(db.Model):
-    __tablename__ = "users"
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    username = db.Column(db.String(128), nullable=False)
-    email = db.Column(db.String(128), nullable=False)
-    active = db.Column(db.Boolean(), default=False, nullable=False)
-    created_at = db.Column(db.DateTime, nullable=False)
+    #set up extensions
+    db.init_app(app)
 
-    def __init__(self, username, email):
-        self.username = username
-        self.email = email
-        self.created_at = datetime.datetime.utcnow()
+    #resister blueprints
+    from project.api.views import users_blueprint
+    app.register_blueprint(users_blueprint)
 
-
-# routes
-
-@app.route('/appsettings',methods=['GET'])
-def appsettings():
-    return app_settings
-
-
-@app.route('/ping', methods=['GET'])
-def ping_pong():
-    return jsonify({
-        'status': 'success',
-        'message': 'pong!'
-    })
-        
+    return app
